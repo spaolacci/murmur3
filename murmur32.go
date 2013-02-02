cb5f2cc2e562f231ad4c2af7a080ff05e146055c
Sébastien Paolacci <sebastien.paolacci@gmail.com>
2013-02-02 10:38:33 +0100

    Initial import.

---
+++ murmur32.go
@@ -0,0 +1,98 @@
+package murmur3
+
+// http://code.google.com/p/guava-libraries/source/browse/guava/src/com/google/common/hash/Murmur3_32HashFunction.java
+
+import (
+	"hash"
+	"unsafe"
+)
+
+// Make sure interfaces are correctly implemented.
+var (
+	_ hash.Hash   = new(digest32)
+	_ hash.Hash32 = new(digest32)
+)
+
+const (
+	c1_32 uint32 = 0xcc9e2d51
+	c2_32 uint32 = 0x1b873593
+)
+
+// digest32 represents a partial evaluation of a 32 bites hash.
+type digest32 struct {
+	digest
+	h1 uint32 // Unfinalized running hash.
+}
+
+func New32() hash.Hash32 {
+	d := new(digest32)
+	d.bmixer = d
+	d.Reset()
+	return d
+}
+
+func (d *digest32) Size() int { return 4 }
+
+func (d *digest32) reset() { d.h1 = 1 }
+
+func (d *digest32) Sum(b []byte) []byte {
+	h := d.h1
+	return append(b, byte(h>>24), byte(h>>16), byte(h>>8), byte(h))
+}
+
+// Digest as many blocks as possible.
+func (d *digest32) bmix(p []byte) (tail []byte) {
+	h1 := d.h1
+
+	nblocks := len(p) / 4
+	for i := 0; i < nblocks; i++ {
+		k1 := *(*uint32)(unsafe.Pointer(&p[i*4]))
+
+		k1 *= c1_32
+		k1 = (k1 << 15) | (k1 >> 17) // rotl32(k1, 15)
+		k1 *= c2_32
+
+		h1 ^= k1
+		h1 = (h1 << 13) | (h1 >> 19) // rotl32(h1, 13)
+		h1 = h1*5 + 0xe6546b64
+	}
+	d.h1 = h1
+	return p[nblocks*d.Size():]
+}
+
+func (d *digest32) Sum32() (h1 uint32) {
+
+	h1 = d.h1
+
+	var k1 uint32
+	switch len(d.tail) & 3 {
+	case 3:
+		k1 ^= uint32(d.tail[2]) << 16
+		fallthrough
+	case 2:
+		k1 ^= uint32(d.tail[1]) << 8
+		fallthrough
+	case 1:
+		k1 ^= uint32(d.tail[0])
+		k1 *= c1_32
+		k1 = (k1 << 15) | (k1 >> 17) // rotl32(k1, 15)
+		k1 *= c2_32
+		h1 ^= k1
+	}
+
+	h1 ^= uint32(d.clen)
+
+	h1 ^= h1 >> 16
+	h1 *= 0x85ebca6b
+	h1 ^= h1 >> 13
+	h1 *= 0xc2b2ae35
+	h1 ^= h1 >> 16
+
+	return h1
+}
+
+/*
+func rotl32(x uint32, r byte) uint32 {
+	return (x << r) | (x >> (32 - r))
+}
+*/
---
+++ murmur_test.go
@@ -0,0 +1,224 @@
+package murmur3
+
+import (
+	"hash"
+	"runtime"
+	"testing"
+)
+
+var data = []struct {
+	h32   uint32
+	h64_1 uint64
+	h64_2 uint64
+	s     string
+}{
+	{0x514e28b7, 0x4610abe56eff5cb5, 0x51622daa78f83583, ""},
+	{0xbb4abcad, 0xa78ddff5adae8d10, 0x128900ef20900135, "hello"},
+	{0x6f5cb2e9, 0x8b95f808840725c6, 0x1597ed5422bd493b, "hello, world"},
+	{0xf50e1f30, 0x2a929de9c8f97b2f, 0x56a41d99af43a2db, "19 Jan 2038 at 3:14:07 AM"},
+	{0x846f6a36, 0xfb3325171f9744da, 0xaaf8b92a5f722952, "The quick brown fox jumps over the lazy dog."},
+}
+
+func TestRef(t *testing.T) {
+	for _, elem := range data {
+
+		var h32 hash.Hash32 = New32()
+		h32.Write([]byte(elem.s))
+		if v := h32.Sum32(); v != elem.h32 {
+			t.Errorf("'%s': 0x%x (want 0x%x)", elem.s, v, elem.h32)
+		}
+
+		var h64 hash.Hash64 = New64()
+		h64.Write([]byte(elem.s))
+		if v := h64.Sum64(); v != elem.h64_1 {
+			t.Errorf("'%s': 0x%x (want 0x%x)", elem.s, v, elem.h64_1)
+		}
+
+		var h128 Hash128 = New128()
+		h128.Write([]byte(elem.s))
+		if v1, v2 := h128.Sum128(); v1 != elem.h64_1 || v2 != elem.h64_2 {
+			t.Errorf("'%s': 0x%x-0x%x (want 0x%x-0x%x)", elem.s, v1, v2, elem.h64_1, elem.h64_2)
+		}
+	}
+}
+
+func TestIncremental(t *testing.T) {
+	for _, elem := range data {
+		h32 := New32()
+		h128 := New128()
+		for i, j, k := 0, 0, len(elem.s); i < k; i = j {
+			j = 2*i + 3
+			if j > k {
+				j = k
+			}
+			s := elem.s[i:j]
+			print(s + "|")
+			h32.Write([]byte(s))
+			h128.Write([]byte(s))
+		}
+		println()
+		if v := h32.Sum32(); v != elem.h32 {
+			t.Errorf("'%s': 0x%x (want 0x%x)", elem.s, v, elem.h32)
+		}
+		if v1, v2 := h128.Sum128(); v1 != elem.h64_1 || v2 != elem.h64_2 {
+			t.Errorf("'%s': 0x%x-0x%x (want 0x%x-0x%x)", elem.s, v1, v2, elem.h64_1, elem.h64_2)
+		}
+	}
+}
+
+//---
+
+func bench32(b *testing.B, length int) {
+	runtime.GC()
+	buf := make([]byte, length)
+	b.SetBytes(int64(length))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		hasher := New32()
+		hasher.Write(buf)
+		hasher.Sum32()
+	}
+}
+
+func Benchmark32_1(b *testing.B) {
+	bench32(b, 1)
+}
+func Benchmark32_2(b *testing.B) {
+	bench32(b, 2)
+}
+func Benchmark32_4(b *testing.B) {
+	bench32(b, 4)
+}
+func Benchmark32_8(b *testing.B) {
+	bench32(b, 8)
+}
+func Benchmark32_16(b *testing.B) {
+	bench32(b, 16)
+}
+func Benchmark32_32(b *testing.B) {
+	bench32(b, 32)
+}
+func Benchmark32_64(b *testing.B) {
+	bench32(b, 64)
+}
+func Benchmark32_128(b *testing.B) {
+	bench32(b, 128)
+}
+func Benchmark32_256(b *testing.B) {
+	bench32(b, 256)
+}
+func Benchmark32_512(b *testing.B) {
+	bench32(b, 512)
+}
+func Benchmark32_1024(b *testing.B) {
+	bench32(b, 1024)
+}
+func Benchmark32_2048(b *testing.B) {
+	bench32(b, 2048)
+}
+func Benchmark32_4096(b *testing.B) {
+	bench32(b, 4096)
+}
+func Benchmark32_8192(b *testing.B) {
+	bench32(b, 8192)
+}
+
+//---
+
+func benchPartial32(b *testing.B, length int) {
+	buf := make([]byte, length)
+	b.SetBytes(int64(length))
+
+	start := (32 / 8) / 2
+	chunks := 7
+	k := length / chunks
+	tail := (length - start) % k
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		hasher := New32()
+		hasher.Write(buf[0:start])
+
+		for j := start; j+k <= length; j += k {
+			hasher.Write(buf[j : j+k])
+		}
+
+		hasher.Write(buf[length-tail:])
+		hasher.Sum32()
+	}
+}
+
+func BenchmarkPartial32_8(b *testing.B) {
+	benchPartial32(b, 8)
+}
+func BenchmarkPartial32_16(b *testing.B) {
+	benchPartial32(b, 16)
+}
+func BenchmarkPartial32_32(b *testing.B) {
+	benchPartial32(b, 32)
+}
+func BenchmarkPartial32_64(b *testing.B) {
+	benchPartial32(b, 64)
+}
+func BenchmarkPartial32_128(b *testing.B) {
+	benchPartial32(b, 128)
+}
+
+//---
+
+func bench128(b *testing.B, length int) {
+	runtime.GC()
+	buf := make([]byte, length)
+	b.SetBytes(int64(length))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		hasher := New128()
+		hasher.Write(buf)
+		hasher.Sum128()
+	}
+}
+
+func Benchmark128_1(b *testing.B) {
+	bench128(b, 1)
+}
+func Benchmark128_2(b *testing.B) {
+	bench128(b, 2)
+}
+func Benchmark128_4(b *testing.B) {
+	bench128(b, 4)
+}
+func Benchmark128_8(b *testing.B) {
+	bench128(b, 8)
+}
+func Benchmark128_16(b *testing.B) {
+	bench128(b, 16)
+}
+func Benchmark128_32(b *testing.B) {
+	bench128(b, 32)
+}
+func Benchmark128_64(b *testing.B) {
+	bench128(b, 64)
+}
+func Benchmark128_128(b *testing.B) {
+	bench128(b, 128)
+}
+func Benchmark128_256(b *testing.B) {
+	bench128(b, 256)
+}
+func Benchmark128_512(b *testing.B) {
+	bench128(b, 512)
+}
+func Benchmark128_1024(b *testing.B) {
+	bench128(b, 1024)
+}
+func Benchmark128_2048(b *testing.B) {
+	bench128(b, 2048)
+}
+func Benchmark128_4096(b *testing.B) {
+	bench128(b, 4096)
+}
+func Benchmark128_8192(b *testing.B) {
+	bench128(b, 8192)
+}
+
+//---
