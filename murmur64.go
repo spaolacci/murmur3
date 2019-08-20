6e5e90107b99cb7c49570d9f328e305602beeff0
yura <funny.falcon@gmail.com>
2019-08-20 16:40:04 +0300

    use inlined implementation of Sum128WithSeed

--- murmur64.go
+++ murmur64.go
@@ -48,10 +48,6 @@ func Sum64(data []byte) uint64 { return Sum64WithSeed(data, 0) }
 //     hasher.Write(data)
 //     return hasher.Sum64()
 func Sum64WithSeed(data []byte, seed uint32) uint64 {
-	d := digest128{h1: uint64(seed), h2: uint64(seed)}
-	d.seed = seed
-	d.tail = d.bmix(data)
-	d.clen = len(data)
-	h1, _ := d.Sum128()
+	h1, _ := Sum128WithSeed(data, seed)
 	return h1
 }
